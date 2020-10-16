fbaa5f492442c6dcf8276c9c57bf15bfebaff140
s3776877 <69615376+s3776877@users.noreply.github.com>
2020-10-16 17:23:36 +1100

    Delete mysql.php

--- AssignmentPart3/website/mysql.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$con=@mysqli_connect("localhost","alphabulider_tee","2YPtwMTm5ep7rKbs","alphabulider_tee");
-@mysqli_query($con,"set names utf8");
-if (!$con) {
-    printf("Can't connect to MySQL Server. Errorcode: %s ", mysqli_connect_error());
-    exit;
-}
